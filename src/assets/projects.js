7e34fb8cf2a410639445cb2fcda94519e4a6210a
kserdult <kserdult@gmail.com>
2023-12-30 00:09:38 +0100

    asd

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -7,7 +7,7 @@ export const data = [
     id: 1,
     image: tictactoe,
     title: "Simple Tic-Tac-Toe game",
-    channel: "Channel",
+    channel: "Project",
     description: "I made this website using HTML, CSS and JavaScript",
     link: "https://kserdult.github.io/simple-tic-tac-toe/",
     sourceCode: "https://github.com/kserdult/simple-tic-tac-toe",
@@ -15,8 +15,8 @@ export const data = [
   {
     id: 2,
     image: placeholder,
-    title: "The BEST Backend Language for You",
-    channel: "ThePrimeTime",
+    title: "Placeholder Placeholder Placeholder",
+    channel: "Placeholder",
     description: "This is a description",
     link: "",
     sourceCode: "",
@@ -24,53 +24,10 @@ export const data = [
   {
     id: 3,
     image: placeholder,
-    title: "Serverless Was A Mistake | Prime Reacts",
-    channel: "ThePrimeTime",
+    title: "Placeholder",
+    channel: "Placeholder",
     description: `
-    The official video 'ARROW OF LOVE' by Electric Callboy starring Kalle
-        Koschinsky! Stream the song & our new album now:
-        https://electriccallboy.lnk.to/Tekkno TEKKNO TOUR 2023: • Tickets:
-        http://www.electriccallboy.com/tickets Order our new Album TEKKNO now:
-        https://electriccallboy.lnk.to/Tekkno Subscribe to our YouTube channel:
-        https://www.youtube.com/c/electriccal... ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ •
-        Merch EU: http://electriccallboy.bravado.de • Merch US:
-        https://merchconnectioninc.com/collec... ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ •
-        Instagram: / electriccallboy • Twitter: / electriccallboy • Facebook: /
-        electriccallboy • TikTok / electriccallboy Music video directed and
-        produced by: Pascal Schillo & Oliver Schillo
-        (http://www.schillobrothers.com) Concept by Schillo Bros & Electric
-        Callboy Film Crew: Florian Berwanger - Cinematography ( / fbwg ) Marvin
-        Dimmerling - Production & BTS Jan Schmidt - Focus Puller Jordy
-        Machmüller - Dolly / Grip Alissa Nieleck - Grip Willy Wilde - Grip Kami
-        Zero - Costume Designer & Make-Up Artist Patrick Unger - Production /
-        Location Scout Music written by Daniel Haniß, Pascal Schillo, Kevin
-        Ratajczak, Nico Sallach Produced by Daniel Haniß, Pascal Schillo, Kevin
-        Ratajczak Mixed by Daniel Haniß ( / danskimo ) Mastered by Christoph
-        Wieczorek ( / sawdustrec ) #ElectricCallboy #ArrowOfLove #TEKKNO LYRICS:
-        I wanna share my heart with everyone around I wanna spread some love I’m
-        feeling the beat I’m gonna give my heart to everyone around I’m gonna
-        share my love with everyone everyone Oh I’m late again My people need me
-        badly Look at Herbert here He’s fighting with his homie I’m fed up with
-        your anger and hate You always fight, punch, bite Through the whole
-        fucking day I give you love Shoot it right into your face My love love
-        is everything that you need Love Is missing in this loveless place I
-        share my heart with everyone I wanna share my heart with everyone around
-        I wanna spread some love I’m feeling the beat I’m gonna give my heart to
-        everyone around I’m gonna share my love with everyone everyone Hey you
-        can’t escape My love is gonna get you And don‘t be afraid ‘cause I don’t
-        wanna hurt you I’m the god of love got the power to heal You from all
-        the sadness and the bad things you feel I give you love Shoot it right
-        into your face My love love is everything that you need Love Is missing
-        in this loveless place I share my heart with everyone I wanna share my
-        heart with everyone around I wanna spread some love I’m feeling the beat
-        I’m gonna give my heart to everyone around I’m gonna share my love with
-        everyone everyone We never stop to believe in magic We‘re diving into
-        the light We never stop to believe in magic We will be forever young
-        ever young I am the one you need I am sent from above To fill your empty
-        hearts with an arrow of love I can not leave Before you give me all your
-        taste, your feels, your love I wanna share my heart with everyone around
-        I wanna spread some love I’m feeling the beat I’m gonna give my heart to
-        everyone around I’m gonna share my love with everyone everyone
+    This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. This is a very long description to see how it handles, long text. This text is repeated multiple times. 
     `,
     link: "",
     sourceCode: "",
@@ -80,7 +37,7 @@ export const data = [
     image: placeholder,
     title:
       "Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title ",
-    channel: "Channel",
+    channel: "Placeholder",
     description: "This is a description",
     link: "",
     sourceCode: "",
@@ -88,36 +45,36 @@ export const data = [
   {
     id: 5,
     image: placeholder,
-    title: "Title",
-    channel: "Channel",
-    description: "This is a description",
+    title: "Placeholder",
+    channel: "Placeholder",
+    description: "Placeholder",
     link: "",
     sourceCode: "",
   },
   {
     id: 6,
     image: placeholder,
-    title: "Title",
-    channel: "Channel",
-    description: "This is a description",
+    title: "Placeholder",
+    channel: "Placeholder",
+    description: "Placeholder",
     link: "",
     sourceCode: "",
   },
   {
     id: 7,
     image: placeholder,
-    title: "Title",
-    channel: "Channel",
-    description: "This is a description",
+    title: "Placeholder",
+    channel: "Placeholder",
+    description: "Placeholder",
     link: "",
     sourceCode: "",
   },
   {
     id: 8,
     image: placeholder,
-    title: "Title",
-    channel: "Channel",
-    description: "This is a description",
+    title: "Placeholder",
+    channel: "Placeholder",
+    description: "Placeholder",
     link: "",
     sourceCode: "",
   },
